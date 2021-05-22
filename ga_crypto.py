f8f1a7eef98599866c108f4349e8e2526c2ecd1d
Yuhao Wu <wyhfrank@gmail.com>
2021-05-23 07:12:38 +0900

    Refactor: extract training code to StrategyTrainer.

--- ga_crypto.py
+++ ga_crypto.py
@@ -6,54 +6,64 @@ from strategy import PurchaseStrategy
 from tools.retrieve_data import get_historical_price
 
 
-def run_ga(df_historical_price, strategy):
-    initial_jpy_asset = 200000
-    crypto_amount = 0
+class StrategyTrainer:
 
-    history = run_simulation(crypto_amount=crypto_amount, assets_jpy=initial_jpy_asset, strategy=strategy,
-                             df_historical_price=df_historical_price)
-    final_state = history[-1]
-    final_total_asset = final_state["total"]
-    earn_rate = (final_total_asset - initial_jpy_asset) / initial_jpy_asset * 100
+    def __init__(self, stg_class=None):
+        self.opt = EaSimpleOptimizer()
+        self.df_historical_price = None
+        self.stg_class = stg_class
 
-    return earn_rate
+    @staticmethod
+    def run_ga(df_historical_price, strategy):
+        initial_jpy_asset = 200000
+        crypto_amount = 0
 
+        history = run_simulation(crypto_amount=crypto_amount, assets_jpy=initial_jpy_asset, strategy=strategy,
+                                 df_historical_price=df_historical_price)
+        final_state = history[-1]
+        final_total_asset = final_state["total"]
+        earn_rate = (final_total_asset - initial_jpy_asset) / initial_jpy_asset * 100
 
-def main():
-    crypto_name = "ETH"
-    start = datetime(2021, 1, 1)
-    # start = datetime(2021, 4, 20)
-    end = datetime(2021, 4, 22)
-    df_historical_price = get_historical_price(crypto_name=crypto_name, start=start, end=end)
+        return earn_rate
 
-    def eval_func(individual):
+    def eval_func(self, individual):
         if not (0 <= individual[0] <= 1 and 0 <= individual[1] <= 1):
             earn_rate = -100
         else:
-            stg = PurchaseStrategy(*individual)
-            earn_rate = run_ga(df_historical_price, strategy=stg)
+            stg = self.stg_class(*individual)
+            earn_rate = self.run_ga(self.df_historical_price, strategy=stg)
         return earn_rate,
 
-    weights = (1.0,)
-    # pop_size = 1000
+    def prepare(self, weights, attr_list, sigma, crypto_name="ETH", start=datetime(2021, 4, 20),
+                end=datetime(2021, 4, 22), pop_size=50, tour_size_factor=0.01, ngen=10):
+
+        mu = [0] * len(attr_list)
+        self.df_historical_price = get_historical_price(crypto_name=crypto_name, start=start, end=end)
+
+        self.opt.prepare(attr_list=attr_list, weights=weights, eval_func=self.eval_func, mu=mu, sigma=sigma,
+                         pop_size=pop_size, tour_size_factor=tour_size_factor, ngen=ngen)
+
+    def train(self, verbose=False):
+        hof, pop, log = self.opt.run(verbose=verbose)
+
+        best = hof[0]
+        stg = self.stg_class(*best)
+        return stg
+
+
+def main():
     pop_size = 50
-    tour_size_factor = 0.01
+    ngen = 10
+    weights = (1.0,)
     attr_list = [[random.uniform, 0, 0.1], [random.uniform, 0.1, 0.2],
                  [random.uniform, 0.00075, 0.005], [random.uniform, 0.1, 5]]
-    mu = [0] * 4
     sigma = [0.2, 0.2, 0.001, 3]
-    ngen = 10
+    st = StrategyTrainer(stg_class=PurchaseStrategy)
+    st.prepare(weights=weights, attr_list=attr_list, sigma=sigma)
+    stg = st.train(verbose=True)
 
-    opt = EaSimpleOptimizer()
-    opt.prepare(attr_list=attr_list, weights=weights, eval_func=eval_func, mu=mu, sigma=sigma,
-                pop_size=pop_size, tour_size_factor=tour_size_factor, ngen=ngen)
-    hof, pop, log = opt.run(verbose=True)
-
-    # print(log)
-    print("hof:{0}".format("\t".join(map(str, hof[0]))))
-    return pop, log, hof
+    print(stg)
 
 
 if __name__ == "__main__":
-    # old()
     main()
