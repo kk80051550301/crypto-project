f91e448a49812d42156eba19db8dac16d8758544
Yuhao Wu <wyhfrank@gmail.com>
2021-05-23 07:12:03 +0900

    Add __str__ to Strategy class.

--- strategy.py
+++ strategy.py
@@ -24,5 +24,14 @@ class PurchaseStrategy:
 
         return invest_index
 
+    def __str__(self):
+        return f"<{self.__class__.__name__}({self.min_thresh:f}, {self.ceiling:f}, {self.a:f}, {self.n:f})>"
 
 
+def test():
+    stg = PurchaseStrategy(0.001, 0.0002, 0.1, 0.2)
+    print(stg)
+
+
+if __name__ == '__main__':
+    test()
