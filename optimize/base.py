a6da4bf8abd57ea24bc91ff179c50065c2c1bad1
Yuhao Wu <wyhfrank@gmail.com>
2021-05-25 21:17:50 +0900

    Prepare for convergence check.

--- optimize/base.py
+++ optimize/base.py
@@ -3,15 +3,69 @@ import random
 from functools import partial
 import numpy
 
-from deap import algorithms
 from deap import base
 from deap import creator
 from deap import tools
+from deap.algorithms import varAnd
 
 """
 Genes: coef (min, max, a, slop)
 Evaluation function: coef: tuple => simulation => earn rate: float
 """
+
+
+def eaSimple(population, toolbox, cxpb, mutpb, ngen, stats=None,
+             halloffame=None, verbose=__debug__):
+    logbook = tools.Logbook()
+    logbook.header = ['gen', 'nevals'] + (stats.fields if stats else [])
+
+    # Evaluate the individuals with an invalid fitness
+    invalid_ind = [ind for ind in population if not ind.fitness.valid]
+    fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
+    for ind, fit in zip(invalid_ind, fitnesses):
+        ind.fitness.values = fit
+
+    if halloffame is not None:
+        halloffame.update(population)
+
+    record = stats.compile(population) if stats else {}
+    logbook.record(gen=0, nevals=len(invalid_ind), **record)
+    if verbose:
+        print(logbook.stream)
+
+    # Begin the generational process
+    for gen in range(1, ngen + 1):
+        # Select the next generation individuals
+        offspring = toolbox.select(population, len(population))
+
+        # Vary the pool of individuals
+        offspring = varAnd(offspring, toolbox, cxpb, mutpb)
+
+        # Evaluate the individuals with an invalid fitness
+        invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
+        fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
+        for ind, fit in zip(invalid_ind, fitnesses):
+            ind.fitness.values = fit
+
+        # Update the hall of fame with the generated individuals
+        if halloffame is not None:
+            halloffame.update(offspring)
+
+        # Replace the current population by the offspring
+        population[:] = offspring
+
+        # Append the current generation statistics to the logbook
+        record = stats.compile(population) if stats else {}
+        logbook.record(gen=gen, nevals=len(invalid_ind), **record)
+        if verbose:
+            print(logbook.stream)
+
+        # TODO: stop here if the max fitness value does not change for several generations. (convergence)
+        # stopped_changing = ...
+        # if stopped_changing:
+        #     break
+
+    return population, logbook
 
 
 class EaSimpleOptimizer:
@@ -46,14 +100,14 @@ class EaSimpleOptimizer:
 
     def run(self, verbose=False):
         start = time.time()
-        pop, log = algorithms.eaSimple(self.pop,
-                                       self.toolbox,
-                                       cxpb=self.cxpb,
-                                       mutpb=self.mutpb,
-                                       ngen=self.ngen,
-                                       stats=self.stats,
-                                       halloffame=self.hof,
-                                       verbose=verbose)
+        pop, log = eaSimple(self.pop,
+                            self.toolbox,
+                            cxpb=self.cxpb,
+                            mutpb=self.mutpb,
+                            ngen=self.ngen,
+                            stats=self.stats,
+                            halloffame=self.hof,
+                            verbose=verbose)
         elapsed = time.time() - start
         if verbose:
             print(f"Time cost: {elapsed:.1f}s")
